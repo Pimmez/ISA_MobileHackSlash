79f3c41bd1f15dbc552a50d34a5853145b096f1a
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-10-22 13:19:20 +0200

    -Removed the standard "SimpleCameraController.cs" -Updated CharacterMovement.cs Somewhat.

--- 2DHackSlash/Assets/Scripts/CharacterMovement.cs
+++ 2DHackSlash/Assets/Scripts/CharacterMovement.cs
@@ -4,16 +4,76 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-		private void Update()
+	[SerializeField] private Joystick joystick;
+	[SerializeField] private float moveSpeed = 40f;
+
+	private float horizontalMovement = 0f;
+	private float verticalMovement = 0f;
+	private bool isJumping = false;
+	private bool isCrouching = false;
+
+	private void Update()
 	{
-		
-	}
-
-
-
-
-#if UNITY_ANDROID
-	Debug.Log("Unity Android Connection Complete");	
+#if UNITY_EDITOR
+		PCMovement();
 #endif
 
+#if UNITY_ANDROID
+		MobileMovement();
+#endif
+	}
+
+	private void PCMovement()
+	{
+		Debug.Log("Hallo Movement"); 
+		horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+
+		transform.Translate(horizontalMovement, 0, 0);
+
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump"))
+		{
+			Debug.Log("Hallo Jump");
+			isJumping = true;
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftControl))
+		{
+			Debug.Log("Hallo Crouch");
+			isCrouching = true;
+		}
+	}
+
+	private void MobileMovement()
+	{
+		if(joystick.Horizontal >= 0.2f)
+		{
+			horizontalMovement = moveSpeed;
+		}
+		else if(joystick.Horizontal <= -0.2f)
+		{
+			horizontalMovement = -moveSpeed;
+		}
+		else
+		{
+			horizontalMovement = 0f;
+		}
+
+		transform.Translate(horizontalMovement, 0, 0);
+
+		verticalMovement = joystick.Vertical;
+
+		if (verticalMovement >= 0.5f)
+		{
+			isJumping = true;
+		}
+
+		if(verticalMovement <= -0.5f)
+		{
+			isCrouching = true;
+		}
+		else
+		{
+			isCrouching = false;
+		}
+	}
 }
